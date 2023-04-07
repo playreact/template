b809c9128d0f3dc070bf2a47ce1050152e788aa0
mancuoj <1337381768@qq.com>
2023-04-07 22:27:44 +0800

    feat: theme change button

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,14 @@
 import Counter from '../Counter'
+import ThemeChange from '../ThemeChange/ThemeChange'
 
 export default function App() {
   return (
     <div className='flex min-h-screen justify-center items-center gap-5'>
-      <h1 className='text-info text-2xl font-serif font-bold'>React Template TS</h1>
-      <Counter />
+      <h1 className='text-info text-2xl font-serif font-bold'>Play React Template</h1>
+      <div className='btn-group'>
+        <Counter />
+        <ThemeChange />
+      </div>
     </div>
   )
 }
