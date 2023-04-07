181e9d36869618f4d26df0231dc36dd85f389d96
mancuoj <1337381768@qq.com>
2023-04-07 22:37:17 +0800

    style: update basic styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,7 @@ import ThemeChange from '../ThemeChange/ThemeChange'
 export default function App() {
   return (
     <div className='flex min-h-screen justify-center items-center gap-5'>
-      <h1 className='text-info text-2xl font-serif font-bold'>Play React Template</h1>
+      <h1 className='text-success text-3xl font-bold font-serif'>Play React Template</h1>
       <div className='btn-group'>
         <Counter />
         <ThemeChange />
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -4,7 +4,7 @@ export default function Counter() {
   const [value, setValue] = useState(0)
 
   return (
-    <button className='btn btn-info btn-square text-lg' onClick={() => setValue(value + 1)}>
+    <button className='btn btn-success btn-square text-lg' onClick={() => setValue(value + 1)}>
       {value}
     </button>
   )
