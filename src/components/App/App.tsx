6145e3ba5a333cc5f0ad15288111d1e31a74b475
mancuoj <1337381768@qq.com>
2023-04-09 03:12:11 +0800

    feat: add footer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,14 @@
 import Counter from '../Counter'
-import ThemeChange from '../ThemeChange'
+import Footer from '../Footer'
 
 export default function App() {
   return (
-    <div className='p-5 flex flex-col min-h-screen justify-center items-center gap-8 md:flex-row'>
+    <div className='flex flex-col min-h-screen justify-center items-center gap-5'>
       <h1 className='text-success text-4xl font-bold font-serif text-center'>
         Play React Template
       </h1>
-      <div className='btn-group font-mono'>
-        <Counter />
-        <ThemeChange />
-      </div>
+      <Counter />
+      <Footer />
     </div>
   )
 }
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -4,7 +4,7 @@ export default function Counter() {
   const [value, setValue] = useLocalStorageState<number>('counter', { defaultValue: 0 })
 
   return (
-    <button className='btn btn-success btn-square text-lg' onClick={() => setValue(value + 1)}>
+    <button className='btn btn-outline btn-success btn-wide text-lg' onClick={() => setValue(value + 1)}>
       {value}
     </button>
   )
