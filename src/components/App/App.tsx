81c83a810b5d8292153f80767e435a8f546520b9
mancuoj <1337381768@qq.com>
2023-04-08 18:06:27 +0800

    style: update basic

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,8 +3,8 @@ import ThemeChange from '../ThemeChange'
 
 export default function App() {
   return (
-    <div className='p-5 flex flex-col min-h-screen justify-center items-center gap-5 md:flex-row'>
-      <h1 className='text-success text-3xl font-bold font-serif text-center'>
+    <div className='p-5 flex flex-col min-h-screen justify-center items-center gap-8 md:flex-row'>
+      <h1 className='text-success text-4xl font-bold font-serif text-center'>
         Play React Template
       </h1>
       <div className='btn-group font-mono'>
