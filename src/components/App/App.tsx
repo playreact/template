273bc4776c7aeb5202e1c4802ada242ebeeed3fe
mancuoj <1337381768@qq.com>
2023-04-15 15:50:58 +0800

    feat: modify toolbar component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,8 +5,7 @@ import Toolbar from '../Toolbar'
 const App: React.FC = () => {
   return (
     <>
-      <Toolbar />
-      <div className='flex flex-col min-h-screen justify-center items-center gap-5'>
+      <div className='flex flex-col min-h-screen justify-center items-center gap-8'>
         <h1 className='text-success text-4xl font-bold font-serif text-center'>
           Play React Template
         </h1>
@@ -15,6 +14,10 @@ const App: React.FC = () => {
           <Counter storageKey='c2' initialValue={2} />
           <Counter storageKey='c3' />
         </div>
+      </div>
+
+      <div className="fixed top-3 right-3">
+        <Toolbar />
       </div>
     </>
   )
